cebb280baa9066878269e9deef6f5abcb764b95f
Giovane Barcelos <giovanebarcelos@gmail.com>
2020-06-02 23:10:51 -0300

    Logoff corrigido

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:gamelist/app/utils/main_drawer.dart';
 import 'package:gamelist/app/utils/routes.dart';
 
-
 class HomePage extends StatefulWidget {
   final String title;
 
@@ -16,7 +15,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,30 +22,33 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text(widget.title),
         actions: <Widget>[
-          Builder(builder: (BuilderContext) {
-
-            return FlatButton(
-              textColor: Theme.of(context).buttonColor,
-              child: Text('Logoff'),
-              onPressed: () async {
-                FirebaseAuth _auth = FirebaseAuth.instance;
-                final FirebaseUser user = await FirebaseAuth.instance.currentUser();
-                if (user == null) {
-                  Scaffold.of(context).showSnackBar(
-                    const SnackBar(content: Text("Ninguem logado."),
-                    ),
-                  );
-                  return;
-                }
-                await _auth.signOut();
-                final String uid = user.uid;
-                Scaffold.of(context).showSnackBar(SnackBar(
-                      content: Text(uid + 'Saiu com Sucesso'),
-                      ));
-                      Navigator.pushReplacementNamed(context, Routes.logon);
-              }, 
+          Builder(
+            builder: (builderContext) {
+              return FlatButton(
+                textColor: Theme.of(context).buttonColor,
+                child: Text('Logoff'),
+                onPressed: () async {
+                  FirebaseAuth _auth = FirebaseAuth.instance;
+                  final FirebaseUser user =
+                      await FirebaseAuth.instance.currentUser();
+                  if (user == null) {
+                    Scaffold.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text("Ninguem logado."),
+                      ),
+                    );
+                    return;
+                  }
+                  await _auth.signOut();
+                  final String uid = user.uid;
+                  Scaffold.of(builderContext).showSnackBar(SnackBar(
+                    content: Text(uid + 'Saiu com Sucesso'),
+                  ));
+                  Navigator.pushReplacementNamed(context, Routes.logon);
+                },
               );
-          },),
+            },
+          ),
         ],
       ),
       body: _body(),
@@ -60,43 +61,39 @@ class _HomePageState extends State<HomePage> {
         width: 500,
         color: Colors.grey[400],
         child: Column(
-      children: <Widget>[
-        _inf("assets/images/HK1.jpg"),
-        _img("assets/images/ORI1.jpg"),    
-      ],
+          children: <Widget>[
+            _inf("assets/images/HK1.jpg"),
+            _img("assets/images/ORI1.jpg"),
+          ],
+        ),
       ),
-    ),
     );
   }
-  _img (String img) {
-  return Image.asset(img,
-    width: 400,
-    height: 400,
-  );
+
+  _img(String img) {
+    return Image.asset(
+      img,
+      width: 400,
+      height: 400,
+    );
   }
 
-  _inf (String img) {
+  _inf(String img) {
     return Container(
       width: 500,
       height: 400,
       color: Colors.grey[400],
       child: Container(
-        child: Column( 
+        child: Column(
           children: <Widget>[
             _img(img),
           ],
-      ),
+        ),
       ),
     );
   }
+
   _text(String text) {
     return Text(text);
   }
-
 }
-
-
-
-
-
-
